f456cd0dad04218ed617e87327a8a5fdc509da41
noahpittsville <npitts1@csub.edu>
2021-12-05 12:58:54 -0800

    Training the model

--- AI-Portion/SA.py
+++ AI-Portion/SA.py
@@ -27,7 +27,36 @@ dataset['text'].head()
 tokenizer = Tokenizer(num_words=5000, split= " ")
 tokenizer.fit_on_texts(dataset['text'].values)
 
-#Now convert the text to a set of arrays to train neural network
+#Now convert the text to a set of arrays that represent the words
 X = tokenizer.texts_to_sequences(dataset['text'].values)
 X = pad_sequences(X)
 print(X[:7])
+
+#Now we do embedding layer
+#This will convert the numbers within the array which is only
+#a single number to vector space. This puts similar words close to one another
+model = Sequential()
+model.add(Embedding(5000, 256, input_length = X.shape[1]))
+
+#We dropout 30%
+model.add(Dropout(0.3))
+model.add(LSTM(256, return_sequences=True, dropout = 0.3, recurrent_dropout=0.2))
+model.add(LSTM(256, dropout = 0.3, recurrent_dropout = 0.2))
+
+#We use dense as 3 because there is positve, negative and neutral
+model.add(Dense(3, activation = 'softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+y = pd.get_dummies(dataset['airline_sentiment']).values
+[print(dataset['airline_sentiment'][i],y[i]) for i in range(0,7)]
+
+#This is the code involved in actually training the model
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 0)
+
+batch_size = 32
+epochs = 10
+print(model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, verbose = 2))
+
+#This is to save the model
+model.save(r"/Users/Noah/Desktop/LSTM/Sentiment/Sentiment_Model.h5")
