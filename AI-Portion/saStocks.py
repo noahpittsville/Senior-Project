efb694746f4f0b30284255ff5d45e2d3a8e9eb75
noahpittsville <npitts1@csub.edu>
2021-12-07 20:30:41 -0800

    Updated sentiment

--- AI-Portion/saStocks.py
+++ AI-Portion/saStocks.py
@@ -1,4 +1,5 @@
 #Sentiment Analysis
+import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,22 +11,29 @@ from keras.preprocessing.text import Tokenizer
 #This helps deal with the different lengths
 from keras.preprocessing.sequence import pad_sequences
 
-dataset = pd.read_csv(r"/Users/Noah/Desktop/Tweets.csv")
+file_name = r"/Users/Noah/Desktop/data/labelled_tweets.csv"
+dataset = pd.read_csv(file_name, sep= ';')
 dataset = dataset.sample(frac=1).reset_index(drop=True)
-dataset.head()
+print(dataset.head())
 dataset.shape
-dataset = dataset[['airline_sentiment', 'text']]
+dataset = dataset[['sentiment', 'text']]
 dataset.head()
 
 #This set of code goes through the code turns everything to lowercase
 #and gets rid of things that aren't letters or numbers
 dataset['text'].apply(lambda x: x.lower())
 dataset['text'] = dataset['text'].apply(lambda x: re.sub('[^a-zA-Z0-9\s]',"",x))
-dataset['text'].head()
+print(dataset['text'].head())
 
 #We only want to keep the most frequent represented by num_words
 tokenizer = Tokenizer(num_words=5000, split= " ")
 tokenizer.fit_on_texts(dataset['text'].values)
+
+#save the tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer,handle,protocol=pickle.HIGHEST_PROTOCOL)
+
+
 
 #Now convert the text to a set of arrays that represent the words
 X = tokenizer.texts_to_sequences(dataset['text'].values)
@@ -48,23 +56,25 @@ model.add(Dense(3, activation = 'softmax'))
 model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-y = pd.get_dummies(dataset['airline_sentiment']).values
-[print(dataset['airline_sentiment'][i],y[i]) for i in range(0,7)]
+#This turns the sentiment values positive, negative and neutral into vectors
+y = pd.get_dummies(dataset['sentiment']).values
+[print(dataset['sentiment'][i],y[i]) for i in range(0,7)]
 
 #This is the code involved in actually training the model
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 0)
 
-batch_size = 32
-epochs = 10
+# batch_size = 32
+# epochs = 10
 
-#This is the code that needs to be ran to fit the model
+# #This is the code that needs to be ran to fit the model
+# print(model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, verbose = 2))
 
-#print(model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, verbose = 2))
+# #This is to save the model
+# model.save(r"/Users/Noah/Desktop/LSTM/Sentiment/small_tweets.h5")
 
-#This is to save the model
-#model.save(r"/Users/Noah/Desktop/LSTM/Sentiment/Sentiment_Model.h5")
+#This is is to load the model once we have it and using it 
 
 #Code to take a look under the hood to see what is predicting correctly and not correctly
 
-#prediction = model.predict(X_test)
-#[print(dataset['text'][i], prediction[i], y_test[i]) for i in range in range(0,7)]
+# prediction = model.predict(X_test)
+# [print(dataset['text'][i], prediction[i], y_test[i]) for i in range in range(0,7)]
