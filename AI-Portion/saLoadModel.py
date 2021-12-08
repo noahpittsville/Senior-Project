9c47982cae7b00bab8591cdc893e99c0c57b5435
noahpittsville <npitts1@csub.edu>
2021-12-08 14:03:16 -0800

    Finishing the code loading

--- AI-Portion/saLoadModel.py
+++ AI-Portion/saLoadModel.py
@@ -31,62 +31,30 @@ dataset['text'].apply(lambda x: x.lower())
 dataset['text'] = dataset['text'].apply(lambda x: re.sub('[^a-zA-Z0-9\s]',"",x))
 print(dataset['text'].head())
 
-#We only want to keep the most frequent represented by num_words
-tokenizer = Tokenizer(num_words=5000, split= " ")
-tokenizer.fit_on_texts(dataset['text'].values)
+tokenizer_location = r"/Users/Noah/Desktop/data/tokenizer.pickle"
 
-#save the tokenizer
-with open('tokenizer.pickle', 'wb') as handle:
-    pickle.dump(tokenizer,handle,protocol=pickle.HIGHEST_PROTOCOL)
-
-# tokenizer_location = r"/Users/Noah/Desktop/data/tokenizer.pickle"
-
-# #Code to load the tokenizer
-# with open(tokenizer_location, 'rb') as handle:
-#     tokenizer = pickle.load(handle)
+#Code to load the tokenizer
+with open(tokenizer_location, 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 #Now convert the text to a set of arrays that represent the words
 X = tokenizer.texts_to_sequences(dataset['text'].values)
 X = pad_sequences(X)
 print(X[:7])
 
-#Now we do embedding layer
-#This will convert the numbers within the array which is only
-#a single number to vector space. This puts similar words close to one another
-model = Sequential()
-model.add(Embedding(5000, 256, input_length = X.shape[1]))
-
-#We dropout 30%
-model.add(Dropout(0.3))
-model.add(LSTM(256, return_sequences=True, dropout = 0.3, recurrent_dropout=0.2))
-model.add(LSTM(256, dropout = 0.3, recurrent_dropout = 0.2))
-
-#We use dense as 3 because there is positve, negative and neutral
-model.add(Dense(3, activation = 'softmax'))
-model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-
 #This turns the sentiment values positive, negative and neutral into vectors
 y = pd.get_dummies(dataset['sentiment']).values
 [print(dataset['sentiment'][i],y[i]) for i in range(0,7)]
 
-#This is the code involved in actually training the model
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.5, random_state = 0)
-
-# batch_size = 32
-# epochs = 8
-
-# #This is the code that needs to be ran to fit the model
-# print(model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, verbose = 2))
-
-# #This is to save the model
-# model.save(r"/Users/Noah/Desktop/LSTM/Sentiment/small_tweets.h5")
-
 #This is is to load the model once we have it and using it 
 model = load_model(r"/Users/Noah/Desktop/LSTM/Sentiment/small_tweets.h5")
 
-#Code to take a look under the hood to see what is predicting correctly and not correctly
-print(X_test)
-prediction = model.predict(X_test)
-[print(dataset['text'][i], prediction[i], y_test[i]) for i in range(0,7)]
+# #Code to take a look under the hood to see what is predicting correctly and not correctly
+# print(X_test)
+# prediction = model.predict(X_test)
+# [print(dataset['text'][i], prediction[i], y_test[i]) for i in range(0,7)]
+
+#Code to attempt to test the entire data set rather than just one set of it
+prediction = model.predict(X)
 print(len(prediction))
+print(prediction[:7])
